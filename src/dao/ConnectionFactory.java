31865813e4ddbf50b474c916240f564c912ac016
Thiago Gentil <88681055+Tgentil@users.noreply.github.com>
2025-05-30 11:03:22 -0300

    chore: ajusta senha padrão da conexão com o db
    closes #4

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -14,7 +14,7 @@ public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/controle_estoque";
     private static final String USER = "root";
-    private static final String PASS = "";
+    private static final String PASS = "teste123";
     
     /**
      * Obtém uma conexão com o banco de dados.
